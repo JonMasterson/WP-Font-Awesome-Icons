4bd5194581790f8b9f3a76f392efb2fc5d71f161
JonMasterson <hello@jonmasterson.com>
2013-06-24 10:51:42 -0400

    Update css

--- add-to-functions.php
+++ add-to-functions.php
@@ -2,7 +2,7 @@
 // Add this to your theme's functions file...
 
 function admin_icons() {
-	wp_register_style( 'admin-ui-icons-css', get_template_directory_uri() . '/css/admin-styles.css', '1.0.0' );
+	wp_register_style( 'admin-ui-icons-css', get_template_directory_uri() . '/css/icon-styles.css', '1.0.0' );
 	wp_enqueue_style( 'admin-ui-icons-css' );
 }
 add_action( 'admin_enqueue_scripts', 'admin_icons' );
