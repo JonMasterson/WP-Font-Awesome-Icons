1108b8bd3e86560baafead75b09878bd772ce77f
Sven <github@hofmannsven.com>
2013-07-23 10:19:59 +0200

    Update functions.php (formatting only)

--- add-to-functions.php
+++ add-to-functions.php
@@ -3,8 +3,8 @@
 
 if ( 'mp6' === get_user_option( 'admin_color' ) ) { // check for MP6 with get_user_option
  function admin_icons() {
-		wp_register_style( 'admin-ui-icons-css', get_template_directory_uri() . '/css/icon-styles.css', false, '1.0.0' );
- 		wp_enqueue_style( 'admin-ui-icons-css' );
- 	}
- 	add_action( 'admin_enqueue_scripts', 'admin_icons' ); // hook into WP admin
+  wp_register_style( 'admin-ui-icons-css', get_template_directory_uri() . '/css/icon-styles.css', false, '1.0.0' );
+   wp_enqueue_style( 'admin-ui-icons-css' );
+ }
+ add_action( 'admin_enqueue_scripts', 'admin_icons' ); // hook into WP admin
 }
